03ff0dbc398c700c8bb8495c58b5d8e557f1e37c
Mohannad-Zeido <mohannad.zeido@gmail.com>
2019-02-24 20:01:11 +0000

    removed error warning

--- app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/AddTeaActivity.java
+++ app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/AddTeaActivity.java
@@ -1,6 +1,5 @@
 package com.zeido.mohannad.timer.tea.teatimer;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
