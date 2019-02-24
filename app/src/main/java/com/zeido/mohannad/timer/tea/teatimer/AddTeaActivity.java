ec85a9e3091b3e54ee6f7c8a14b299a7604acb6f
Mohannad-Zeido <mohannad.zeido@gmail.com>
2019-02-24 19:49:45 +0000

    implemnt add tea page
    The new tea is saved in memory and will get deleted when app shutsdown

--- app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/AddTeaActivity.java
+++ app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/AddTeaActivity.java
@@ -1,11 +1,20 @@
 package com.zeido.mohannad.timer.tea.teatimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.zeido.mohannad.timer.tea.teatimer.Database.SampleDataProvider;
+import com.zeido.mohannad.timer.tea.teatimer.Database.Tea;
+
+import java.util.List;
 
 public class AddTeaActivity extends AppCompatActivity {
+
+    private EditText mTeaName, mBrewingTime, mTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,10 @@ public class AddTeaActivity extends AppCompatActivity {
 
         Button submitButton = findViewById(R.id.submitTea);
         Button cancelButton = findViewById(R.id.cancelTea);
+        mTeaName = findViewById(R.id.teaNameEditText);
+        mBrewingTime = findViewById(R.id.teaBrewingTimeEditText);
+        //todo make sure time gets converted to millis
+        mTemperature = findViewById(R.id.teaTemperatureEditText);
 
 
         submitButton.setOnClickListener(submitButtonListener);
@@ -23,14 +36,23 @@ public class AddTeaActivity extends AppCompatActivity {
     private View.OnClickListener submitButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //todo add to array and return to homePage
+
+            List<Tea> teaList = SampleDataProvider.teaList;
+            String teaName = mTeaName.getText().toString();
+            long brewingTime = Long.parseLong(mBrewingTime.getText().toString());
+            int temperature = Integer.parseInt(mTemperature.getText().toString());
+            teaList.add(new Tea(teaName,"", brewingTime, temperature,null));
+            //Todo validate the text sent by user
+            setResult(RESULT_OK);
+            finish();
         }
     };
 
     private View.OnClickListener cancelButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //todo return to homePage
+            setResult(RESULT_CANCELED);
+            finish();
         }
     };
 }
--- app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/HomePageActivity.java
+++ app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/HomePageActivity.java
@@ -1,11 +1,13 @@
 package com.zeido.mohannad.timer.tea.teatimer;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.zeido.mohannad.timer.tea.teatimer.Database.SampleDataProvider;
 import com.zeido.mohannad.timer.tea.teatimer.Database.Tea;
@@ -14,7 +16,8 @@ import java.util.List;
 
 public class HomePageActivity extends AppCompatActivity {
 
-    List<Tea> teaList = SampleDataProvider.teaList;
+    private List<Tea> teaList = SampleDataProvider.teaList;
+    private TeaItemAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +29,23 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), AddTeaActivity.class);
-                view.getContext().startActivity(intent);
+                startActivityForResult(intent, 100);
             }
         });
 
-        TeaItemAdapter adapter = new TeaItemAdapter(this, teaList);
+        mAdapter = new TeaItemAdapter(this, teaList);
 
         RecyclerView recyclerView = findViewById(R.id.mainRecyclerView);
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(mAdapter);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            mAdapter.notifyDataSetChanged();
+            Toast.makeText(this, "Tea Added", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
