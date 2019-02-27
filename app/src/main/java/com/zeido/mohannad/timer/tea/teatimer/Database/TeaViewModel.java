f1c34b565ff1a66d7cef15b61dac58dcf314f392
Mohannad Zeido <mohannad.zeido@gmail.com>
2019-02-27 09:29:16 +0000

    Created View Model

---
+++ app/src/main/java/com/zeido/mohannad/timer/tea/teatimer/Database/TeaViewModel.java
@@ -0,0 +1,27 @@
+package com.zeido.mohannad.timer.tea.teatimer.Database;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TeaViewModel extends AndroidViewModel {
+
+    private TeaRepository mTeaRepository;
+    private LiveData<List<Tea>> mAllTeas;
+
+    public TeaViewModel(Application application){
+        super(application);
+        mTeaRepository = new TeaRepository(application);
+        mAllTeas = mTeaRepository.getmAllTeas();
+    }
+
+    public LiveData<List<Tea>> getmAllTeas(){
+        return mAllTeas;
+    }
+
+    public void insetr(Tea tea){
+        mTeaRepository.insert(tea);
+    }
+}
